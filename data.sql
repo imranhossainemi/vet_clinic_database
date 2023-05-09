9fe9e3ce27f6724febdadeb678bce11d5f164645
Imran Hossain <blur-imran@hotmail.com>
2023-05-10 00:56:54 +0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -32,3 +32,66 @@ INSERT INTO animals (
   weight_kg
   ) 
   VALUES ('Devimon', date '2017-05-12', 5, true, 11);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Charmander', date '2020-02-08', 0, false, -11);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Plantmon', date '2021-11-15', 2, true, -5.7);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Squirtle', date '1993-04-02', 3, false, -12.13);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Angemon', date '2005-06-12', 1, true, -45);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Boarmon', date '2005-06-07', 7, true, 20.4);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Blossom', date '1998-10-13', 3, true, 17);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+  ) 
+  VALUES ('Ditto', date '2022-05-14' , 4, true, 22);
