850aaa5b6c60711bbf4ce72c434dc0e82fd01d03
Imran Hossain <blur-imran@hotmail.com>
2023-05-10 00:57:04 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -23,3 +23,91 @@ SELECT * from animals WHERE name != 'Gabumon';
 
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)*/
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.*/
+
+BEGIN;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+/*
+Inside a transaction:
+
+    Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+    Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+    Verify that changes were made.
+    Commit the transaction.
+    Verify that changes persist after commit
+*/
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+/*
+Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+After the rollback verify if all records in the animals table still exists. After that, you can start breathing as usual ;)
+*/
+BEGIN;
+
+DELETE FROM animals;
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+/*
+Inside a transaction:
+
+    Delete all animals born after Jan 1st, 2022.
+*/
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals;
+
+-- Create a savepoint for the transaction.
+SAVEPOINT del_date;
+
+--Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO SAVEPOINT del_date;
+SELECT * FROM animals;
+
+--Update all animals' weights that are negative to be their weight multiplied by -1. Commit transaction
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- How many animals are there?
+SELECT COUNT(id) FROM animals;
+
+--How many animals have never tried to escape?
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '1999-12-31';
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+--Who escapes the most, neutered or not neutered animals?
+SELECT MAX(escape_attempts) FROM animals WHERE neutered = false OR true;
+
+--What is the minimum and maximum weight of each type of animal?
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals;
