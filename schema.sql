7a22a9f0da8dda5f5ae08daf2c25f71c147f3d5f
Imran Hossain <blur-imran@hotmail.com>
2023-05-14 00:03:56 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -17,8 +17,8 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(spcies_id) ON DELETE CASCADE;
 
 ALTER TABLE animals ADD COLUMN owners_id INT, ADD CONSTRAINT fk_owner FOREIGN KEY (owners_id) REFERENCES owners(owners_id) ON DELETE CASCADE;
--- Owners table
 
+-- Owners table
 CREATE TABLE owners (
   owners_id BIGSERIAL NOT NULL PRIMARY KEY,
   full_name VARCHAR(100) NOT NULL,
@@ -30,4 +30,30 @@ CREATE TABLE species(
   spcies_id BIGSERIAL NOT NULL PRIMARY KEY,
   species_name VARCHAR(100)
 );
+
+--Vets table
+CREATE TABLE vets (
+  vet_id: BIGSERIAL NOT NULL PRIMARY KEY,
+  vet_name: VARCHAR(100) NOT NULL,
+  vet_age: INT NOT NULL,
+  date_of_graduation: date NOT NULL
+);
+
+CREATE TABLE specializations (
+  spe_id: BIGSERIAL NOT NULL PRIMARY KEY,
+  vet_id: BIGSERIAL NOT NULL,
+  species_id: BIGSERIAL NOT NULL,
+  ADD CONSTRAINT fk_vet_id FOREIGN KEY (vet_id) REFERENCES vets(vet_id) ON DELETE CASCADE;
+  ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(spcies_id) ON DELETE CASCADE;
+);
+
+CREATE TABLE visits (
+  visit_id: BIGSERIAL NOT NULL PRIMARY KEY,
+  vet_id: BIGSERIAL NOT NULL,
+  animals_id: BIGSERIAL NOT NULL,
+  date_of_visit: date NOT NULL,
+  ADD CONSTRAINT fk_vet_id FOREIGN KEY (vet_id) REFERENCES vets(vet_id) ON DELETE CASCADE;
+  ADD CONSTRAINT fk_animals_id FOREIGN KEY (animals_id) REFERENCES animals(id) ON DELETE CASCADE;
+);
+
 
